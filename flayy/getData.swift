cd33cf91810cee4a69ce212105b813802d09451b
kato <kvldez@centraldealarmas.com.mx>
2017-07-18 09:47:46 -0500

    Ready

---
+++ flayy/getData.swift
@@ -0,0 +1,26 @@
+//
+//  getData.swift
+//  flayy
+//
+//  Created by Jose Katzuo Valdez Carmona on 7/5/17.
+//  Copyright © 2017 CAMSA. All rights reserved.
+//
+
+import AVKit
+import Firebase
+
+class getData: NSObject {
+    
+    public var ref: DatabaseReference!
+    
+    public func getData(user: String){
+        
+        
+    }
+    
+    public func sendData(){
+        
+    }
+    
+    
+}
