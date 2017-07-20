e0b04431268ae24ad9bcd4ff146b13341de82c4e
kato <kvldez@centraldealarmas.com.mx>
2017-07-20 09:10:25 -0500

    ni idea pour no se subieron

---
+++ flayy/PlacesMap.swift
@@ -0,0 +1,21 @@
+//
+//  PlacesMap.swift
+//  flayy
+//
+//  Created by Jose Katzuo Valdez Carmona on 7/19/17.
+//  Copyright © 2017 CAMSA. All rights reserved.
+//
+
+import UIKit
+
+class PlacesMap: GMSMapView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ flayy/PlacesMapViewController.swift
@@ -0,0 +1,69 @@
+//
+//  PlacesMapViewController.swift
+//  flayy
+//
+//  Created by Jose Katzuo Valdez Carmona on 7/19/17.
+//  Copyright © 2017 CAMSA. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class PlacesMapViewController: UIViewController,  GMSMapViewDelegate{
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let locValue: CLLocationCoordinate2D = locationManager.location!.coordinate
+        let marker = GMSMarker()
+        let marcador = UIImage(named: "ic28_casita.jpg")
+        let markerView = UIImageView(image: resizeImage(image: marcador!, newSize: CGSize(width: 35, height: 38)))
+
+        markerView.layer.borderWidth = 1
+        markerView.layer.masksToBounds = false
+        markerView.backgroundColor = UIColor.blue
+        markerView.clipsToBounds = true
+        
+        marker.position = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+        marker.title = "Kato"
+        marker.iconView = markerView
+        
+        let camera = GMSCameraPosition.camera(withLatitude: locValue.latitude, longitude: locValue.longitude, zoom: 15.0, bearing: -15, viewingAngle: 45)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        
+        marker.map = mapView
+        
+        self.view = mapView
+    }
+    
+    func resizeImage(image: UIImage, newSize: CGSize) -> UIImage {
+        
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        let context = UIGraphicsGetCurrentContext()
+        
+        // Set the quality level to use when rescaling
+        context!.interpolationQuality = CGInterpolationQuality.default
+        let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+        
+        context!.concatenate(flipVertical)
+        // Draw into the context; this scales the image
+        context?.draw(image.cgImage!, in: CGRect(x: 0.0,y: 0.0, width: newRect.width, height: newRect.height))
+        
+        let newImageRef = context!.makeImage()! as CGImage
+        let newImage = UIImage(cgImage: newImageRef)
+        
+        // Get the resized image from the context and a UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+}
