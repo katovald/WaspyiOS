229e5d5def57c27252a547305660be3fcff9b63b
kato <kvldez@centraldealarmas.com.mx>
2017-08-22 12:17:29 -0500

    deleted

--- Pods/Target Support Files/Pods-flayyTests/Pods-flayyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flayyTests : NSObject
-@end
-@implementation PodsDummy_Pods_flayyTests
-@end
--- Pods/Target Support Files/Pods-flayyUITests/Pods-flayyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flayyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_flayyUITests
-@end
